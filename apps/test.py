0704971bbbbf57007d43954c71f2ca3206e1539f
Stefan Bauer <stefanjakob.bauer@gmail.com>
2023-12-17 16:16:12 +0100

    feat: test notebook and script added

---
+++ apps/test.py
@@ -0,0 +1,18 @@
+"""
+This is a test file to check if the PySpark kernel is working.
+"""
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Testing PySpark Example").getOrCreate()  # type: ignore
+
+sample_data = [
+    {"name": "John D.", "age": 30},
+    {"name": "Alice  G.", "age": 25},
+    {"name": "Bob T.", "age": 35},
+    {"name": "Eve A.", "age": 28},
+]
+
+df = spark.createDataFrame(sample_data)
+
+print(df.show())  # type: ignore
